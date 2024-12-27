9c8deeb389e80d37721a5c8004e2a839e7e43f7b
Adam <doktoradam72@gmail.com>
2024-12-27 20:05:50 +0500

    Initial Commit

---
+++ newProject9/newProject9App.swift
@@ -0,0 +1,17 @@
+//
+//  newProject9App.swift
+//  newProject9
+//
+//  Created by Адам Дотмерзаев on 23.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct newProject9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
